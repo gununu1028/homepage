e662d296b3533a0a6ab83855ed6366fb0a8b6371
松田淳司 <amatsuda@zaico.co.jp>
2020-01-01 13:14:35 +0900

    Feat: bulma 導入

--- md/.vuepress/config.js
+++ md/.vuepress/config.js
@@ -1,5 +1,13 @@
-
 module.exports = {
-  base: '/official-blog/',
-  dest: process.cwd() + '/docs/'
-}
+  base: "/official-blog/",
+  dest: process.cwd() + "/docs/",
+  head: [
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"
+      }
+    ]
+  ]
+};
