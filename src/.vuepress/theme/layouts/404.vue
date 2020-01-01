c7ef37befc363472d83be17f5c1fb9078f5c8e44
松田淳司 <amatsuda@zaico.co.jp>
2020-01-01 17:53:26 +0900

    Fix: 日本語にした

--- src/.vuepress/theme/layouts/404.vue
+++ src/.vuepress/theme/layouts/404.vue
@@ -1,26 +1,9 @@
 <template>
   <div class="theme-container">
     <div class="theme-default-content">
-      <h1>404</h1>
-      <blockquote>{{ getMsg() }}</blockquote>
-      <router-link to="/">Take me home.</router-link>
+      <h1>404 Not Found</h1>
+      <blockquote>お探しのページは見つかりませんでした。</blockquote>
+      <router-link to="/">トップページへ</router-link>
     </div>
   </div>
 </template>
-
-<script>
-const msgs = [
-  `There's nothing here.`,
-  `How did we get here?`,
-  `That's a Four-Oh-Four.`,
-  `Looks like we've got some broken links.`
-]
-
-export default {
-  methods: {
-    getMsg () {
-      return msgs[Math.floor(Math.random() * msgs.length)]
-    }
-  }
-}
-</script>
