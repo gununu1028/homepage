488e2e93d58ee392199b1f5928781bc4373971f5
松田淳司 <amatsuda@zaico.co.jp>
2020-03-02 16:07:25 +0900

    Fix: 文字の大きさなどを調整

--- src/.vuepress/theme/components/Page.vue
+++ src/.vuepress/theme/components/Page.vue
@@ -14,26 +14,33 @@
 <style lang="stylus">
 p
   margin-bottom 1rem
-  
-h2 
+
+h2
   margin-bottom 1rem
 </style>
 
 <script>
-import PageEdit from '@theme/components/PageEdit.vue'
-import PageNav from '@theme/components/PageNav.vue'
+import PageEdit from "@theme/components/PageEdit.vue";
+import PageNav from "@theme/components/PageNav.vue";
 
 export default {
   components: { PageEdit, PageNav },
-  props: ['sidebarItems']
-}
+  props: ["sidebarItems"]
+};
 </script>
 
 <style lang="stylus">
 @require '../styles/wrapper.styl';
 
-.page {
-  padding-bottom: 2rem;
-  display: block;
-}
+.page
+  padding-bottom: 2rem
+  display block
+@media (max-width: $MQMobile)
+  .content
+    h1 
+      font-size 1.5rem
+    h2
+      font-size 1.25rem
+    h3
+      font-size 1.1rem
 </style>
