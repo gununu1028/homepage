0aed3bd03d46bd55e746e5ec9cb4790776113d17
松田淳司 <amatsuda@zaico.co.jp>
2020-03-13 12:39:49 +0900

    Fix: 画像のパスを修正

--- src/.vuepress/components/ProfileCard.vue
+++ src/.vuepress/components/ProfileCard.vue
@@ -3,16 +3,16 @@
     <article class="media">
       <div class="media-left">
         <figure class="image is-128x128">
-          <img src="/assets/img/profile.bacc1f3b.jpg" alt="松田淳司の写真" />
+          <img src="../profile.jpg" alt="松田淳司の写真">
         </figure>
       </div>
       <div class="media-content">
         <dl>
           <dt>松田淳司（まつだあつし）</dt>
           <dd>
-            山形県出身／在住<br />
-            山形県立産業技術短期大学校卒業後、地元のIT企業に就職。自治体ウェブサイトの構築やLinuxサーバーの運用業務を行う。<br />
-            現在はWebアプリケーションエンジニアとしてクラウド在庫管理ソフトの開発に従事。<br />
+            山形県出身／在住<br>
+            山形県立産業技術短期大学校卒業後、地元のIT企業に就職。自治体ウェブサイトの構築やLinuxサーバーの運用業務を行う。<br>
+            現在はWebアプリケーションエンジニアとしてクラウド在庫管理ソフトの開発に従事。<br>
             高校生や短大生への技術指導にも力を入れており、技能五輪全国大会や若年者ものづくり競技大会へ出場する学生を指南している。
           </dd>
         </dl>
