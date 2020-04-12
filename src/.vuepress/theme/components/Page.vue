710ef8a46cefdda780a947d8975d92b4223431a6
松田淳司 <amatsuda@zaico.co.jp>
2020-04-13 00:05:44 +0900

    Style: 余白を調整

--- src/.vuepress/theme/components/Page.vue
+++ src/.vuepress/theme/components/Page.vue
@@ -1,48 +1,54 @@
 <template>
-  <main class="page">
-    <slot name="top" />
+    <main class="page">
+        <slot name="top"/>
 
-    <Content class="theme-default-content content" />
-    <PageEdit />
+        <Content class="theme-default-content content"/>
+        <PageEdit/>
 
-    <PageNav v-bind="{ sidebarItems }" />
+        <PageNav v-bind="{ sidebarItems }"/>
 
-    <slot name="bottom" />
-  </main>
+        <slot name="bottom"/>
+    </main>
 </template>
 
 <style lang="stylus">
-p
-  margin-bottom 1rem
+    p
+        margin-bottom 1rem
 
-h2
-  margin-bottom 1rem
+    h2
+        margin-bottom 1rem
 </style>
 
 <script>
-import PageEdit from "@theme/components/PageEdit.vue";
-import PageNav from "@theme/components/PageNav.vue";
+    import PageEdit from "@theme/components/PageEdit.vue";
+    import PageNav from "@theme/components/PageNav.vue";
 
-export default {
-  components: { PageEdit, PageNav },
-  props: ["sidebarItems"]
-};
+    export default {
+        components: {PageEdit, PageNav},
+        props: ["sidebarItems"]
+    };
 </script>
 
 <style lang="stylus">
-@require '../styles/wrapper.styl';
+    @require '../styles/wrapper.styl';
 
-.page
-  padding-bottom: 2rem
-  display block
-@media (max-width: $MQMobile)
-  .content
-    h1 
-      font-size 1.5rem
-    h2
-      font-size 1.25rem
-    h3
-      font-size 1.1rem
-    ul 
-      margin-left 0.5rem
-</style>
+    .page
+        padding-bottom: 2rem
+        display block
+
+    @media (max-width: $MQMobile)
+        .content
+            h1
+                font-size 1.5rem
+
+            h2
+                font-size 1.25rem
+
+            h3
+                font-size 1.1rem
+
+            ul
+                margin-left 0.5rem
+
+    <
+    /style >
